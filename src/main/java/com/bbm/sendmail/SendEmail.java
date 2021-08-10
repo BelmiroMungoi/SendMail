b66678616ea096877200ad50728529ed5290be2a
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-08-10 10:48:40 +0200

    classe para o envio de email

---
+++ src/main/java/com/bbm/sendmail/SendEmail.java
@@ -0,0 +1,66 @@
+package com.bbm.sendmail;
+
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author bbm29
+ */
+public class SendEmail {
+
+    private String userName = "belmiroteste@gmail.com";
+    private String password = "";
+    private String listDestination = "";
+    private String subject = "";
+    private String content = "";
+    private String name = "";
+
+    public SendEmail(String listDestination, String subject, String content, String name) {
+        this.listDestination = listDestination;
+        this.subject = subject;
+        this.content = content;
+        this.name = name;
+    }
+
+    public void sendEmail() {
+
+        try {
+            Properties properties = new Properties();
+
+            properties.put("mail.smtp.ssl.trust", "*");
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.starttls", "true");
+            properties.put("mail.smtp.host", "smtp.gmail.com");
+            properties.put("mail.smtp.port", "465");
+            properties.put("mail.smtp.socketFactory.port", "465");
+            properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+            Session session = Session.getInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(userName, password);
+                }
+
+            });
+
+            Address[] toUser = InternetAddress.parse(listDestination);
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(userName, name));
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject(subject);
+            message.setText(content);
+            Transport.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
